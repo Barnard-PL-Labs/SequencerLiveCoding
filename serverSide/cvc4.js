a8f245b82b89ff3a1a18cf3d20e7554b4d539637
Mark Santolucito <mark.santolucito@yale.edu>
2021-12-10 10:56:20 -0500

    fix logging

--- serverSide/cvc4.js
+++ serverSide/cvc4.js
@@ -63,9 +63,9 @@ exports.callPBE = function (examples) { //examples :: [[Int]]
   //  callCVC4 
   //}
 
-  fs.writeFile('logs/' + Date.now() + '.sl', cvc4Query, (err) => {
+  /*fs.writeFile('logs/' + Date.now() + '.sl', cvc4Query, (err) => {
     if (err) throw err;
-  })
+  })*/
   //for this to work on mac, might need timeout like https://github.com/santolucito/liveprogramming/blob/ba690f1354abe2580fb5e0ce7484eb1379a3ed6a/lib/javascript/eval_pbe_helpers.js#L46
   timeoutLength = 1;
   var cvc4Command = 'doalarm () { perl -e \'alarm shift; exec @ARGV\' "$@"; }\n doalarm ' + timeoutLength + ' bash -c \"echo \\"' + cvc4Query + '\\" | /usr/local/bin/cvc4 --lang sygus2\"';
