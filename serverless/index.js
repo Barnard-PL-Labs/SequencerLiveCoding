eeebd6e51345ab56bc0d28f863fc4023eb35312c
Mark Santolucito <msantolu@barnard.edu>
2022-03-25 13:42:33 -0400

    add caching to serverless synthesis

--- serverless/index.js
+++ serverless/index.js
@@ -1,16 +1,11 @@
 const { exec, execSync } = require('child_process');
 const fs = require('fs');
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 callPBEserverless = function (cvc4Query) { //examples :: String (in sygus format)
 
   var sygusOutput = ""
-
-  //if (cvc4Query in database) {
-  //  sygusOutput = lookupResult(cvc4Query)
-  //}
-  //else {
-  //  do stuff below
-  //}
 
   timeoutLength = 1;
   var cvc4Command = 'bash -c \"echo \\"' + cvc4Query + '\\" | ./cvc5 --lang sygus2\"';
@@ -39,9 +34,59 @@ exports.handler = async (event) => {
     console.log("trying to read as API call")
     data = JSON.parse(event.body).query;
   }
+  synthResponse = '';
+  queryInDB= false;
+  try {
+    synthResponse = await docClient.get(constructQuery(data)).promise();
+    synthResponse = synthResponse.Item.response;
+    queryInDB = true;
+  } catch (err) {
+    console.log(err);
+    console.log('didnt find query in db, running synth instead')
+    synthResponse = JSON.stringify(callPBEserverless(data));
+  }
   const response = {
       statusCode: 200,
-      body: JSON.stringify(callPBEserverless(data)),
+      body: synthResponse,
   };
+  if (!queryInDB) {
+    try {
+      console.log('didnt find query in db, saving result (even a negative result) for later')
+      await addToTable(data, response.body)
+    } catch (err) {
+      console.log(err);
+      return { error: err }
+    }
+  }
   return response;
 };
+
+function constructQuery(q) {
+  return (
+   { TableName : 'CVC5_queries',
+       Key: {
+          query: q
+       }
+   });
+}
+
+function constructItem(q,r) {
+  return (
+   { TableName : 'CVC5_queries',
+       Item: {
+          query: q,
+          response: r
+       }
+   });
+}
+
+async function addToTable(query, response){
+  console.log("here");
+  try {
+    await docClient.put(constructItem(query, response)).promise();
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
