6c0fa4506fb4401e944b9ccaf04850c6f7b52030
Justin Kim <jtk2141@columbia.edu>
2022-03-29 14:29:05 -0400

    changed scope of message

--- public/js/synthesis.js
+++ public/js/synthesis.js
@@ -96,11 +96,11 @@ function updatePatternFromCode(currentBeat, trackIndex, globalTime) {
     let fxnDef = 'function genBeat(b, s, currentTimestep, globalTime){\n\n ';
     let returnStatement = 'return {beat:b, sliders:s};\n};';
     let fxnText = '"use strict"; ' + dsl + fxnDef +  updatedCode + returnStatement + ' return (genBeat(theBeat, {}, trackIndex, globalTime));'
+    var message = document.getElementById("errorMessageBox");
     try {
         //TODO if(codeChanged) {
         let f = new Function("theBeat", "trackIndex", "globalTime", fxnText);
         let newData = f(currentBeat, trackIndex, globalTime);
-        var message = document.getElementById("errorMessageBox");
         if (message.innerText = "Code looks good!"){
             message.style.color = "green";
         }
@@ -118,15 +118,16 @@ function updatePatternFromCode(currentBeat, trackIndex, globalTime) {
             //console.log(newBeat);
             return { beat: newBeat, sliders: newSliders };
         }
-
     }
+
     catch (err) {
         console.log("updatePatternFromCode error, skipping beat state update");
         console.log(err);
-        var message = document.getElementById("errorMessageBox");
         if (message.innerText = err){
             message.style.color = "red";
         }
+
+
     }
     return null;
 }
